ceb125c53dd7ef8566084b228474ee07a2253887
gigug <62102058+gigug@users.noreply.github.com>
2025-02-20 09:15:17 +0000

    Deploying to gh-pages from @ gigug/gigug.github.io@5e26a244d3ed5f22ff842392e0701bb089a4df0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,6 +438,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/gigu", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=6oklbVUAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
